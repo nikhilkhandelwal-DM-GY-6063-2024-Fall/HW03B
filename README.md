0070f8fbfc6bc9a2898c4727db115930610e4628
nikhil0119 <nikhilkhandelwal0119@gmail.com>
2024-10-04 01:42:13 -0400

    code added for the optical spiral

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# HW03-A
+# HW03-B
 
-**This is a submission of Homework for Week 3 Part A.**
+**This is a submission of Homework for Week 3 Part B.**
+
+In this homework I tried to create a rotational geometric spiral pattern using squares. 
+
+ It features alternating black and white squares, gradually rotating and scaling down toward the center. 
+ 
+ This type of design is known for its visual appeal, and it's commonly used in abstract or optical art to create the illusion of movement and depth.
 
 Link:  
 [Homework 3 Link](https://6063b.github.io/homework/03/)
-
-Image Link
-[Image](https://6063b.github.io/homework/03/#pid=1)
 
 Image on repo:  
 ![image description](https://github.com/nikhilkhandelwal-DM-GY-6063-2024-Fall/HW03A/blob/main/Homework_WK03_A.jpg)
--- sketch.js
+++ sketch.js
@@ -1,27 +1,22 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(235,214,1);
+  background(255);
+  
+  translate(width/2,height/2); //center of the canvas
 
-  // For the Bigger Circles
-  for (let y = 0; y < height; y += 100) {
-    for (let x = 0; x < width; x += 100) {
-      fill(0,0,0);
-      ellipse(x, y, 30);
+  let size=300; //starting size of rectangle
+
+  for(let i=0; i<20; i++){
+    if(i%2==0){ //for filling alternate color
+      fill(0);
+    } else{
+      fill(255);
     }
-  }
-  // For the smaller circles
-  for (let y = 0; y < height; y += 50) {
-    for (let x = 0; x < width; x += 50) {
-      fill(0,0,0);
-      ellipse(x, y, 10);
-    }
-  }
-  // For the bigger circles in second alternate rows
-  for (let y = 50; y < height; y += 100) {
-    for (let x = 50; x < width; x += 100) {
-      fill(0,0,0);
-      ellipse(x, y, 30);
-    }
+
+    rotate(PI/10);
+    rectMode(CENTER);
+    rect(0,0,size,size);
+    size=size*0.85;
   }
 
 }
